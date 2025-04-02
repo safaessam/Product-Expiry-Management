a4ea80a7e4efb8e5074c1b5560b7320003309a80
Safa Sababty <safaessameldeensaid@gmail.com>
2025-04-02 11:33:17 +0200

    [IMP]pxm:add module structure

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+  {
+    'name': 'Product Expiry Management',
+    'version': "1.0.0",
+    'summary': 'Manage product expiry dates and notifications',
+    'description': """
+        This module adds expiry date tracking to products and sends notifications
+        when products are about to expire.
+    """,
+    'author': 'Safa Essam',
+    'category': 'Inventory',
+    'depends': ['product', 'stock', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_view.xml',
+        'data/ir_cron.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
