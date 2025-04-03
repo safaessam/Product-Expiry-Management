d8899beaaa5de48239db85fa11ff2b4154473ebe
Safa Sababty <safaessameldeensaid@gmail.com>
2025-04-03 11:22:33 +0200

    [IMP]pxm:add model

--- pxm/models/product.py
+++ pxm/models/product.py
@@ -3,19 +3,28 @@
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
+from odoo import models, fields, api, _
+from datetime import datetime, timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
-class ObjectModel(models.Model):
-
+class ProductTemplate(models.Model):
     # region ---------------------- TODO[IMP]: Private Attributes --------------------------------
-    _name = "module.model"
-    _description = "model template"
+    _inherit = 'product.template'
+    _description = 'Product Template'
     # endregion
 
     # region ---------------------- TODO[IMP]:Default Methods ------------------------------------
     # endregion
 
     # region ---------------------- TODO[IMP]: Fields Declaration ---------------------------------
+
+    expiry_date = fields.Date(string='Expiry Date')
+    is_expired = fields.Boolean(string='Is Expired', compute='_compute_is_expired', store=False)
+    is_near_expiry = fields.Boolean(string='Near Expiry', compute='_compute_is_near_expiry', store=False)
+
     # region  Basic
     # endregion
 
@@ -30,6 +39,21 @@ class ObjectModel(models.Model):
 
     # endregion
     # region ---------------------- TODO[IMP]: Compute methods ------------------------------------
+
+    @api.depends('expiry_date')
+    def _compute_is_expired(self):
+       pass
+
+    @api.depends('expiry_date')
+    def _compute_is_near_expiry(self):
+
+       pass
+
+    def check_expiry_products(self):
+        """Method to check for products nearing expiry and send notifications"""
+
+
+
     # endregion
 
     # region ---------------------- TODO[IMP]: Constrains and Onchanges ---------------------------
